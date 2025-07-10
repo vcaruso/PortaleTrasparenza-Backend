cb9368e6e4424601d846544bd660c3641383e725
vincenzo caruso <vincenzo.caruso@external.enilive.com>
2025-07-10 16:28:34 +0200

    aggiunta latitudine e longitudine

--- ENINET.TransaprentPortal.Persistence/Migrations/20250710090857_Add Latitute and Longitude to Site.Designer.cs
+++ ENINET.TransaprentPortal.Persistence/Migrations/20250710090857_Add Latitute and Longitude to Site.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ENINET.TransaprentPortal.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ENINET.TransparentPortal.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250710090857_Add Latitute and Longitude to Site")]
+    partial class AddLatituteandLongitudetoSite
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ENINET.TransaprentPortal.Persistence/Migrations/20250710090857_Add Latitute and Longitude to Site.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ENINET.TransparentPortal.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLatituteandLongitudetoSite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Sites",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Sites",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.UpdateData(
+                table: "Sites",
+                keyColumn: "Acronym",
+                keyValue: "RO",
+                columns: new[] { "Latitude", "Longitude" },
+                values: new object[] { 0.0, 0.0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Sites");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Sites");
+        }
+    }
+}
