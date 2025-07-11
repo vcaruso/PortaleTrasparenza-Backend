992ce4447aac9f3aac385d8d96cde3a7b4395244
vincenzo caruso <vincenzo.caruso@external.enilive.com>
2025-07-11 16:00:01 +0200

    Filtro elemento aggiunto

--- src/ENINET.TransparentPortal.API/Controllers/DashboardController.cs
+++ src/ENINET.TransparentPortal.API/Controllers/DashboardController.cs
@@ -15,15 +15,15 @@ namespace ENINET.TransparentPortal.API.Controllers
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         }
-        [HttpGet("reportformonths/{acronym}/{year}")]
+        [HttpGet("reportformonths/{acronym}/{year}/{elementName}")]
 
         [ProducesResponseType(typeof(ApiResult<IList<ElementStatisticDto>>), 200)]
-        public async Task<ApiResult<IList<ElementStatisticDto>>> GetYearReport(string acronym, int year)
+        public async Task<ApiResult<IList<ElementStatisticDto>>> GetYearReport(string acronym, int year, string elementName)
         {
             var email = User.Claims.Where(t => t.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name").FirstOrDefault();
             var authorizedSites = User.Claims.Where(t => t.Type == "TransparentSites").Select(s => s.Value).ToArray();
             var statisticsReports = await _repository.Report.FindByCondition(null, r => r.Acronym.ToLower() == acronym.ToLower() && authorizedSites.Contains(r.Acronym) && r.Year == year.ToString(), false).ToListAsync();
-            var elements = await _repository.ElementSite.FindByCondition(null, r => r.Acronym.ToLower() == acronym.ToLower() && authorizedSites.Contains(r.Acronym), false).ToListAsync();
+            var elements = await _repository.ElementSite.FindByCondition(null, r => r.Acronym.ToLower() == acronym.ToLower() && authorizedSites.Contains(r.Acronym) && r.ElementName == elementName, false).ToListAsync();
             var statistics = new List<ElementStatisticDto>();
             var months = Enumerable.Range(1, 12);
             foreach (var element in elements)
